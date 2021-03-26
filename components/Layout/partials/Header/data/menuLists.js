25a2e54c3c42a591d716fb42e8cff453866dc313
Abraham Carvajal <acarvajal@saatvamattress.com>
2021-03-26 00:55:43 -0600

    Add matress menu

--- components/Layout/partials/Header/data/menuLists.js
+++ components/Layout/partials/Header/data/menuLists.js
@@ -5,54 +5,55 @@
 const MenuList = [
     {
         title: 'Mattresses',
+        defaultImage: 'd-nav-mattresses@1x.jpg',
         subCategories: [
             {
                 title: 'FEATURED',
+                separator: true,
                 items: [
                     {
                         title: 'Saatva Classic',
-                        image: 'images/d-nav-saatva-classic@1x.jpg',
+                        image: 'd-nav-saatva-classic@1x.jpg',
                         description: 'Luxury Hybrid Innerspring'
                     },
                     {
                         title: 'Loom & Leaf',
-                        image: 'images/navigation/d-nav-loom-and-leaf@1x.jpg',
+                        image: 'd-nav-loom-and-leaf@1x.jpg',
                         description: 'Ultra Premium Memory Foam'
                     },
                     {
                         title: 'Saatva Latex Hybrid',
-                        image: 'images/navigation/d-nav-saatva-latex-hybrid@1x.jpg',
+                        image: 'd-nav-saatva-latex-hybrid@1x.jpg',
                         description: '100% Natural and Organic'
                     },
                     {
                         title:  'Solaire Adjustable',
-                        image: 'images/navigation/d-nav-solaire@1x.jpg',
+                        image: 'd-nav-solaire@1x.jpg',
                         description: 'Customizable comfort'
                     }
                 ]
             },
             {
                 title: 'INNERSPRING',
-                separator: true,
                 items: [
                     {
                         title: 'Saatva Classic',
-                        image: 'images/d-nav-saatva-classic@1x.jpg',
+                        image: 'd-nav-saatva-classic@1x.jpg',
                         description: 'Luxury Hybrid Innerspring'
                     },
                     {
                         title: 'Saatva Latex Hybrid',
-                        image: 'images/navigation/d-nav-saatva-latex-hybrid@1x.jpg',
+                        image: 'd-nav-saatva-latex-hybrid@1x.jpg',
                         description: '100% Natural and Organic'
                     },
                     {
                         title: 'Saatva Youth',
-                        image: 'images/navigation/d-nav-saatva-youth@1x.jpg',
+                        image: 'd-nav-saatva-youth@1x.jpg',
                         description: 'Dual-sided Innerspring for Growing Bodies'
                     },
                     {
                         title:  'Saatva HD',
-                        image: 'images/navigation/d-nav-saatva-hd@1x.jpg',
+                        image: 'd-nav-saatva-hd@1x.jpg',
                         description: 'Luxury Hybrid for Bigger Bodies'
                     }
                 ]
@@ -62,7 +63,7 @@ const MenuList = [
                 items: [
                     {
                         title: 'Loom & Leaf',
-                        image: 'images/navigation/d-nav-loom-and-leaf@1x.jpg',
+                        image: 'd-nav-loom-and-leaf@1x.jpg',
                         description: 'Ultra Premium Memory Foam'
                     }
                 ]
@@ -72,12 +73,12 @@ const MenuList = [
                 items: [
                     {
                         title:  'Solaire Adjustable',
-                        image: 'images/navigation/d-nav-solaire@1x.jpg',
+                        image: 'd-nav-solaire@1x.jpg',
                         description: 'Customizable comfort'
                     },
                     {
                         title: 'Zenhavean Latex',
-                        image: 'images/navigation/d-nav-zenhaven@1x.jpg',
+                        image: 'd-nav-zenhaven@1x.jpg',
                         description: '100% Natural Talalay Latex'
                     }
                 ]
@@ -86,13 +87,20 @@ const MenuList = [
         ]
     },
     {
-        title: 'Bedding'
+        title: 'Bedding',
+        defaultImage: '',
+        subCategories: []
     },
     {
-        title: 'Frame & Bases'
+        title: 'Frame & Bases',
+        defaultImage: '',
+        subCategories: []
     },
     {
-        title: 'Sale'
+        title: 'Sale',
+        defaultImage: '',
+        subCategories: [],
+        featured: true
     }
 ]
 
--- components/Layout/partials/Header/subcomponents/index.js
+++ components/Layout/partials/Header/subcomponents/index.js
@@ -1,3 +1,5 @@
 export { default as Logo } from './Logo'
 export { default as Menu } from './Menu'
 export { default as MenuIcons } from './MenuIcons'
+export { default as MenuSection } from './MenuSection'
+
